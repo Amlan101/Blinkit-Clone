21c3dc38b8a3f2b159c8d9513c19eff00a5b0633
Amlan101 <amlan.sahooricky@gmail.com>
2025-02-19 20:57:10 +0530

    Initial commit with Splash Screen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:blinkit/repository/screens/splash/slpashscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Blinkit Clone',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: SplashScreen()
+    );
+  }
+}
